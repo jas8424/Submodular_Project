b1606c40c5ab0bf1cbdc46dbb78d157d8c6148a7
jas8424 <138172467+jas8424@users.noreply.github.com>
2024-04-20 00:22:06 -0700

    Update disr_sample_cont_greedy.py

--- disr_sample_cont_greedy.py
+++ disr_sample_cont_greedy.py
@@ -8,39 +8,39 @@ import random
 # sample_rate is number of clieatns or professors we sample at each round--> better name it client_sample
 
 
-def dist_sample_cont_greedy(A_to_B, B_to_A, k=20, p=50, sample_rate=0.2):
-    # Initializing
+def dist_sample_cont_greedy(A_to_B, B_to_A, t=40, k=20, num_gradient_estimation_samples=20, client_sample = 0.1):
     A_length, B_length = len(A_to_B), len(B_to_A)
-    sampled_A = int(A_length * sample_rate)
+    sampled_A = int(A_length * client_sample)
     vect_x = np.zeros(B_length)
     A_to_B_items = list(A_to_B.items())
-    # t = communication_rounds for instance 40, 50, 
-    # for _ in range (t):
-    #    ...
 
-    
-    for _ in range(k): # k here is the number of iterartion of communications
-        gradient_matrix = np.zeros((sampled_A, B_length))
+    for _ in range(t):
+        gradient_matrix = np.zeros((sampled_A, B_length), dtype=np.int8)
         matrix_v = np.zeros((sampled_A, B_length), dtype=np.bool_)
-        # Sampled professors
         sampled_AB_items = random.sample(A_to_B_items, sampled_A)
-
-        # Estimate the gradient, assume it's correct, explain later
-        for _ in range(p):
+        
+        for _ in range(num_gradient_estimation_samples):
             sampled_Bs = np.nonzero(np.random.rand(B_length) <= vect_x)[0]
-
             for i, (key, A_to_B_value) in enumerate(sampled_AB_items):
                 intersect_index = np.intersect1d(A_to_B_value, sampled_Bs, assume_unique=True)
                 if not intersect_index.size:
-                    gradient_matrix[i, np.setdiff1d(A_to_B_value, sampled_Bs, assume_unique=True)] += 1 / p
+                    gradient_matrix[i, np.setdiff1d(A_to_B_value, sampled_Bs, assume_unique=True)] += 1
                 elif len(intersect_index) == 1:
-                    gradient_matrix[i, intersect_index] += 1 / p
+                    gradient_matrix[i, intersect_index] += 1
 
-        # Update vector_v
-        top_k_indices = np.argsort(-gradient_matrix, axis=1)[:, :k] # k here should be the number of conferences! for example 3, or 5 or 
+        # Apply random shuffle to indices
+        shuffled_indices = np.random.permutation(B_length)
+
+        # Apply argpartition to get top k indices in shuffled order
+        partitioned_indices = np.argpartition(-gradient_matrix[:, shuffled_indices], kth=k-1, axis=1)[:, :k]
+
+        # Convert shuffled partitioned indices back to original indices
+        original_top_k_indices = shuffled_indices[partitioned_indices]
+
         row_indices = np.arange(sampled_A)[:, None]
-        matrix_v[row_indices, top_k_indices] = 1
+        matrix_v[row_indices, original_top_k_indices] = 1
         vect_v = np.mean(matrix_v, axis=0)
 
-        vect_x += 1.0 / k * vect_v # k here is the number of communication rounds
+        vect_x += 1.0 / t * vect_v
+        
     return vect_x
